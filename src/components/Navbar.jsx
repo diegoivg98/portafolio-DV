f137e63482d35a51f3d524a414ef9b97c9b6953d
Diego Vistoso <diegoivg98@gmail.com>
2023-03-13 17:27:44 -0300

    agregar estilos, iconos y useState a componente Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
+import Logo from "../assets/Logo.jpg";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+    const [navbar, setNavbar] = useState(false);
+    const handleClick = () => setNavbar(!navbar)
+    
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#1472ac] text-neutral-900">
+      {/* LOGO */}
+      <div>
+        <img
+          src={Logo}
+          alt="logo"
+          style={{ width: "90px", heigt: "90px", borderRadius: "100px" }}
+        />
+      </div>
+
+      {/* Menu */}
+      <ul className="hidden md:flex">
+        <li>Inicio</li>
+        <li>Sobre mi</li>
+        <li>Tecnologias</li>
+        <li>Proyectos</li>
+      </ul>
+
+      {/* Menu Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        { !navbar ? <FaBars/>:<FaTimes/> }
+      </div>
+
+      {/* Menu mobile */}
+      <ul className={!navbar ? 'hidden':'absolute top-0 left-0 w-full h-screen bg-[#1472ac] flex flex-col justify-center items-center'}>
+        <li className="py-6 text-4xl">Inicio</li>
+        <li className="py-6 text-4xl">Sobre mi</li>
+        <li className="py-6 text-4xl">Tecnologias</li>
+        <li className="py-6 text-4xl">Proyectos</li>
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
