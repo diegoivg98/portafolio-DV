666eb41abc200bd322b11ebbe81d651827b96513
Diego Vistoso <diegoivg98@gmail.com>
2023-03-16 21:41:15 -0300

    cambios de font y resumen de textos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,15 +6,14 @@ const Home = () => {
     <div name="home" className="w-full h-screen bg-[#23b5d3]">
       {/* Container */}
       <div className="max-w[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-stone-900">Hola, mi nombre es:</p>
+        <p className="text-stone-900 font-mono">Hola, mi nombre es:</p>
         <h1 className="text-4xl sm:text-7xl font-bold">Diego Vistoso</h1>
         <h2 className="text-4xl sm:text-7xl font-bold">
           Desarrollador Full Stack.
         </h2>
-        <p className="py-4 max-w[700px]">
-          Tengo habilidades en HTML, CSS, Sass, JavaScript, Bootstrap, PHP,
-          Codeigniter, MVC, Git y bases de datos MySQL y PostgreSQL. Con 8 meses
-          de experiencia en el campo, estoy en constante aprendizaje y
+        <p className="py-4 max-w[700px] font-sans">
+          Tengo habilidades en la creación y desarrollo de sitios web dinámicos
+          utilizando diversas tecnologías y herramientas de programación. En constante aprendizaje y
           actualmente estoy adquiriendo conocimientos en Laravel y ReactJS en{" "}
           <strong>Open Bootcamp</strong>
         </p>
