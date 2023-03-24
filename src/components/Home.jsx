da6b21657bb14cc2906f84e45591a63b8fe55f2f
Diego Vistoso <diegoivg98@gmail.com>
2023-03-23 21:59:31 -0300

    arreglo menor en el div container

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#23b5d3]">
       {/* Container */}
-      <div className="max-w[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-stone-900 font-mono">Hola, mi nombre es:</p>
         <h1 className="text-4xl sm:text-7xl font-bold">Diego Vistoso</h1>
         <h2 className="text-4xl sm:text-7xl font-bold">
@@ -13,9 +13,9 @@ const Home = () => {
         </h2>
         <p className="py-4 max-w[700px] font-sans">
           Tengo habilidades en la creación y desarrollo de sitios web dinámicos
-          utilizando diversas tecnologías y herramientas de programación. En constante aprendizaje y
-          actualmente estoy adquiriendo conocimientos en Laravel y ReactJS en{" "}
-          <strong>Open Bootcamp</strong>
+          utilizando diversas tecnologías y herramientas de programación. En
+          constante aprendizaje y actualmente estoy adquiriendo conocimientos en
+          Laravel y ReactJS en <strong>Open Bootcamp</strong>
         </p>
         <div>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-gray-900 hover:border-gray-900">
