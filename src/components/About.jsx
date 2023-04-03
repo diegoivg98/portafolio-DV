fc471c329e91f4cea9948351514f25c6bb79ce35
Diego Vistoso <diegoivg98@gmail.com>
2023-04-03 01:06:49 -0400

    cambio menor en los parrafos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-red-700">
+            <p className="text-4xl font-bold inline border-b-4 text-gray-900 border-slate-900">
               Acerca de
             </p>
           </div>
@@ -20,7 +20,7 @@ const About = () => {
             </p>
           </div>
           <div>
-            <p className="font-mono">
+            <p className="font-mono text-gray-900">
               Titulado Ingeniería Ejecución en Computación e Informática y
               también como Técnico de Nivel Superior en Programación y Análisis
               de Sistemas. Capacitado para realizar labores de análisis de
